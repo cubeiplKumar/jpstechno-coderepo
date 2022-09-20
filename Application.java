51a6294667df896efa4994bb73118f938f6acf41
cubeiplkumar <emailanilkumarm@gmail.com>
2022-09-20 09:26:25 +0530

    feature301 changes by ashish

--- Application.java
+++ Application.java
@@ -22,4 +22,12 @@ class Main {
       e.getStackTrace();
     }
   }
+#feature301 changes by Developer Ashish o sept2022
+// reads all data from input.txt
+      sourceFile.read(array);
+
+      // writes all data to newFile
+      destFile.write(array);
+      System.out.println("The input.txt file is copied to newFile.");
+  
 }
